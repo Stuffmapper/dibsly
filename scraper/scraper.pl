7a25c591f46c6dd0756227b5d75eb840dc0606a9
faltantornillos <faltantornillos@gmail.com>
2014-04-24 10:22:37 -0700

    including all big cities

--- scraper/scraper.pl
+++ scraper/scraper.pl
@@ -10,49 +10,119 @@ use HTML::Grabber;
 use Data::Dumper;
 use Tie::IxHash;
 
-my %dataHash;
+my %domains;
 
-tie %dataHash, 'Tie::IxHash';
+tie %domains, 'Tie::IxHash';
 
-my $domain = 'http://seattle.craigslist.org';
+$domains{'http://sfbay.craigslist.org'} = 0;
+$domains{'http://albany.craigslist.org'} = 0;
+$domains{'http://albuquerque.craigslist.org'} = 0;
+$domains{'http://anchorage.craigslist.org'} = 0;
+$domains{'http://atlanta.craigslist.org'} = 0;
+$domains{'http://austin.craigslist.org'} = 0;
+$domains{'http://baltimore.craigslist.org'} = 0;
+$domains{'http://boise.craigslist.org'} = 0;
+$domains{'http://boston.craigslist.org'} = 0;
+$domains{'http://buffalo.craigslist.org'} = 0;
+$domains{'http://charlotte.craigslist.org'} = 0;
+$domains{'http://chicago.craigslist.org'} = 0;
+$domains{'http://cincinnati.craigslist.org'} = 0;
+$domains{'http://cleveland.craigslist.org'} = 0;
+$domains{'http://columbus.craigslist.org'} = 0;
+$domains{'http://dallas.craigslist.org'} = 0;
+$domains{'http://denver.craigslist.org'} = 0;
+$domains{'http://detroit.craigslist.org'} = 0;
+$domains{'http://fresno.craigslist.org'} = 0;
+$domains{'http://norfolk.craigslist.org'} = 0;
+$domains{'http://hartford.craigslist.org'} = 0;
+$domains{'http://honolulu.craigslist.org'} = 0;
+$domains{'http://houston.craigslist.org'} = 0;
+$domains{'http://indianapolis.craigslist.org'} = 0;
+$domains{'http://kansascity.craigslist.org'} = 0;
+$domains{'http://lasvegas.craigslist.org'} = 0;
+$domains{'http://losangeles.craigslist.org'} = 0;
+$domains{'http://louisville.craigslist.org'} = 0;
+$domains{'http://memphis.craigslist.org'} = 0;
+$domains{'http://milwaukee.craigslist.org'} = 0;
+$domains{'http://minneapolis.craigslist.org'} = 0;
+$domains{'http://nashville.craigslist.org'} = 0;
+$domains{'http://neworleans.craigslist.org'} = 0;
+$domains{'http://newyork.craigslist.org'} = 0;
+$domains{'http://oklahomacity.craigslist.org'} = 0;
+$domains{'http://omaha.craigslist.org'} = 0;
+$domains{'http://orangecounty.craigslist.org'} = 0;
+$domains{'http://orlando.craigslist.org/'} = 0;
+$domains{'http://philadelphia.craigslist.org/'} = 0;
+$domains{'http://phoenix.craigslist.org/'} = 0;
+$domains{'http://pittsburgh.craigslist.org/'} = 0;
+$domains{'http://portland.craigslist.org/'} = 0;
+$domains{'http://raleigh.craigslist.org/'} = 0;
+$domains{'http://providence.craigslist.org/'} = 0;
+$domains{'http://richmond.craigslist.org/'} = 0;
+$domains{'http://sacramento.craigslist.org/'} = 0;
+$domains{'http://saltlakecity.craigslist.org/'} = 0;
+$domains{'http://sanantonio.craigslist.org/'} = 0;
+$domains{'http://sandiego.craigslist.org/'} = 0;
+$domains{'http://seattle.craigslist.org/'} = 0;
+$domains{'http://miami.craigslist.org/'} = 0;
+$domains{'http://stlouis.craigslist.org/'} = 0;
+$domains{'http://tampa.craigslist.org/'} = 0;
+$domains{'http://tucson.craigslist.org/'} = 0;
+$domains{'http://washingtondc.craigslist.org/'} = 0;
 
-my $nextLink = '/see/zip/';
 
-my $keepPaginatingBoolean = 1;
+@months = qw( Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec );
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+my $todayKey = $months[$mon]." ".$mday;
 
-while ($keepPaginatingBoolean) {
 
-    print @links[0,1];
+while(my($domainsKey, $domainsValue) = each %domains){
+    my %dataHash;
 
-    my $mech = WWW::Mechanize->new();
-    print "grabbing ".$domain.$nextLink."\n";
-    $mech->get($domain.$nextLink);
+    tie %dataHash, 'Tie::IxHash';
 
-    my $dom = HTML::Grabber->new( html => $mech->content );
+    my $domain = $domainsKey;
 
-    $dom->find('div.content')->each(sub {
-        $_->find('p.row')->each(sub {
-            @words = ($_->text =~ /(\w+)/g);
-            $key = @words[0]." ".@words[1];
-            if (exists $dataHash{$key}) {
-                push ($dataHash{$key}, $_->text);
-            } else {
-                $dataHash{$key} = [$_->text];
-            }
+    my $nextLink = '/zip/';
+
+    my $keepPaginatingBoolean = 1;
+
+    while ($keepPaginatingBoolean) {
+
+        print @links[0,1];
+
+        my $mech = WWW::Mechanize->new();
+        print "grabbing ".$domain.$nextLink."\n";
+        $mech->get($domain.$nextLink);
+
+        my $dom = HTML::Grabber->new( html => $mech->content );
+
+        $dom->find('div.content')->each(sub {
+            $_->find('p.row')->each(sub {
+                $date = $_->find('span.date')->text;
+                if (exists $dataHash{$date}) {
+                    push ($dataHash{$date}, $_->text);
+                } else {
+                    $dataHash{$date} = [$_->text];
+                }
+            });
         });
-    });
 
-    $dom->find('div.bottompaginator a.next')->each(sub {
-        $nextLink = $_->attr('href');
-    });
+        $dom->find('a.next')->each(sub {
+            $nextLink = $_->attr('href');
+        });
 
-    if ((scalar (keys %dataHash)) > 6) {
-        $keepPaginatingBoolean = 0;
+        if ((scalar (keys %dataHash)) > 1) {
+            $keepPaginatingBoolean = 0;
+        }
+
+        print Dumper(\%dataHash);
+    }
+
+    if (exists $dataHash{$todayKey}) {
+        $domains{$domainsKey} = scalar grep defined($_),values $dataHash{$todayKey};
     }
 }
 
-print Dumper(\%dataHash);
-
-
-
+print Dumper(\%domains);
 
