b2636b1e28b5b2e4370fee0e4c73cd4b46c0b7f6
faltantornillos <faltantornillos@gmail.com>
2014-08-18 20:31:08 -0700

    floats

--- db/migrate/20140217062614_create_posts.rb
+++ db/migrate/20140217062614_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.string :title, :null => false
+      t.string :title
       t.text :description
       t.string :image_url
       t.string :address
