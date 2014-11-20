c9a502e52d9f1b72ef8b0f8d1c510cc2f516bd7a
BenRuns <benthomaspatterson@gmail.com>
2014-11-20 12:30:22 -0800

    changed captcha key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Leo0vgSAAAAAHu-_4bT70rAbA9e3fTAaZc4mQg_'
-  config.private_key = '6Leo0vgSAAAAAM49CNca9qyfrhVDdAy5fVHIRfsl'
+  config.public_key  = '6Lf7I_4SAAAAAPGiBimtbPv2Tu5oZFWS2NMGGUTZ'
+  config.private_key = '6Lf7I_4SAAAAAPQfAjConrEtT-8h30aEzRZC_RAp'
+
 end
