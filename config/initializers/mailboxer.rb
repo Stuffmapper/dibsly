fec8d548bc51cb5bdb8fd33ffc8b120434996b01
BenRuns <benthomaspatterson@gmail.com>
2015-04-02 10:00:49 -0700

    turning off emails until tested

--- config/initializers/mailboxer.rb
+++ config/initializers/mailboxer.rb
@@ -1,6 +1,6 @@
 Mailboxer.setup do |config|
   #Enables or disables email sending for Notifications and Messages
-  config.uses_emails = true
+  config.uses_emails = false
   #Configures the default `from` address for the email sent for Messages and Notifications of Mailboxer
   config.default_from = "no-reply@dit.upm.es"
  
