0ca18bca7de82e99505f6fc6a4b4c68ebb57d7c2
BenRuns <benthomaspatterson@gmail.com>
2015-04-23 10:10:34 -0700

    cucs passing

--- features/step_definitions/posting.rb
+++ features/step_definitions/posting.rb
@@ -165,7 +165,7 @@ Then(/^I should be able to click edit and change the details$/) do
   click_button 'Edit'
   fill_in 'description', with: "I have changed the details"
   click_button "Update"
-  sleep(1)
+  sleep(2)
   expect(page.body).to have_text('Your post has been updated') # express the regexp above with the code you wish you had
   @shoes = @post 
   steps %{
