c8a8d67a1f742f81d38d37bc3a49b124e1231406
BuildTools <jessebannon@live.com>
2015-04-26 11:15:46 -0700

    Unique usernames added, linked to Facebook Stuffmapper dev page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '461364914027300', '35fa23a059266def0ab1b9719d66e4b3'
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
