8b5ea6fb2e493a1fd9f028fe8b601176fca23496
Ben Patterson <benthomaspatterson@gmail.com>
2015-06-17 10:17:20 -0700

    fixed failing test

--- features/step_definitions/user_accounts_steps.rb
+++ features/step_definitions/user_accounts_steps.rb
@@ -7,14 +7,14 @@ end
 Given(/^that I am using the same email for my google and facebook$/) do
  	@facebook_email = 'fake@email.com'
  	@google_email = 'fake@email.com'
- 	@email = 'fake@email.com'   
-  #see test.rb in environment to make sure 
+ 	@email = 'fake@email.com'
+  #see test.rb in environment to make sure
   #that omniauth google and fb emails are configured to match
 end
 
 
 Given(/^that I am not logged in and don't have an account$/) do
-  #do nothing  
+  #do nothing
 end
 
 
@@ -24,7 +24,7 @@ end
 
 
 
-Given(/^that I have the signup page open$/) do 
+Given(/^that I have the signup page open$/) do
 
 	# express the regexp above with the code you wish you had
   visit('/')
@@ -33,7 +33,7 @@ end
 
 
 Given(/^I signup with a username password email and phone$/) do
- 
+
   fill_in 'username', with: 'fakeuser'
   fill_in 'first_name', with: 'Ben'
   fill_in 'last_name', with: 'Dere'
@@ -41,9 +41,9 @@ Given(/^I signup with a username password email and phone$/) do
   fill_in 'password', with: "123456"
   fill_in 'password_confirmation', with: "123456"
   fill_in 'phone_number', with: "8675309"
-  within('.modal-footer') do 
+  within('.modal-footer') do
     click_button "Sign Up"
-  end  
+  end
   sleep(1)
 end
 
@@ -78,9 +78,9 @@ end
 
 Given(/^that I already have an account$/) do
 
-  
+
   @current_user = create(:user, :last_name  => 'Name',
-              :first_name => 'fake', 
+              :first_name => 'fake',
               :username   => 'fakeuser',
               :password   => '123456',
               :email      => 'fake@email.com',
@@ -92,7 +92,7 @@ end
 
 Then(/^I should expect to see a "(.*?)"$/) do |policy|
   click_link(policy)
-   expect(page.body).to have_text("This is a " + policy )  
+   expect(page.body).to have_text("This is a " + policy )
 end
 
 
@@ -112,11 +112,11 @@ end
 Given(/^I fail to sign in with the password "(.*?)"$/) do |arg1|
   fill_in 'username', with: 'Jack'
   fill_in 'password', with: arg1
-  within('.modal-footer') do 
+  within('.modal-footer') do
     click_button 'Sign In'
   end
   sleep(1)
-  expect(page.body).to have_text("Wrong username or password")  
+  expect(page.body).to have_text("Wrong username or password")
 end
 
 Then(/^I should see a forgot password\? link$/) do
@@ -125,13 +125,13 @@ Then(/^I should see a forgot password\? link$/) do
 end
 
 When(/^I follow the forgot password link and enter my email$/) do
-  click_button('Forgot Password?') 
+  click_button('Forgot Password?')
   fill_in 'email', with: @user.email
 
-  
+
   click_button "Reset"
   sleep(2)
-  
+
 end
 
 Then(/^I should receive an email with a link to reset my password$/) do
@@ -143,7 +143,7 @@ end
 
 When(/^I follow the reset password link and set my new password to "(.*?)"$/) do |arg1|
   current_email.click_link "http://"
-  expect(page).to have_content("Change Your Password") 
+  expect(page).to have_content("Change Your Password")
   fill_in 'password', with: arg1
   fill_in 'password_confirmation', with: arg1
   click_button 'Change Password'
@@ -156,7 +156,7 @@ Then(/^I should be able to login with my username and "(.*?)"$/) do |arg1|
     first(:link, 'Sign In').click
     fill_in 'username', with: @user.username
     fill_in 'password', with: arg1
-    within('.modal-footer') do 
+    within('.modal-footer') do
        click_button 'Sign In'
     end
     expect(page).to have_text("You have been signed in")
@@ -167,7 +167,7 @@ When(/^I try to login, I should be able to use my email in place of username$/) 
     first(:link,'Sign In').click
     fill_in 'username', with: @current_user.email
     fill_in 'password', with: @current_user.password
-    within('.modal-footer') do 
+    within('.modal-footer') do
        click_button 'Sign In'
     end
     sleep(1)
@@ -180,19 +180,19 @@ end
 Then(/^I should receive a welcome email$/) do
   sleep(1)
   @current_user = User.last
-  open_email(@current_user.email) 
+  open_email(@current_user.email)
   expect(current_email.body).to have_text("Welcome to Stuffmapper!" )
 
 end
 
 When(/^I follow the link in the welcome email$/) do
   current_email.click_link "http://"
-  expect(page).to have_content("You've verified your email!") 
+  expect(page).to have_content("You've verified your email!")
 end
 
 Then(/^I should be able to post an item and dib Jacks shoes$/) do
   click_link('Sign Out')
-  
+
   steps %{
     When I log in and give stuff
     Then I should be able to put  "These are some awesome kicks" in the description field
@@ -209,7 +209,7 @@ Then(/^I should be able to post an item and dib Jacks shoes$/) do
   page.all(:button, "Dib")[0].click
   sleep(1)
   expect(page).to have_text('Dibbed your stuff')
- 
+
  end
 
 
@@ -217,24 +217,23 @@ Then(/^I should be able to post an item and dib Jacks shoes$/) do
 When(/^I sign in I should not be able to dib Jack's shoes or post an item\.$/) do
   visit '/'
   click_link('Sign Out')
-  @current_user = User.last 
+  @current_user = User.last
   steps %{
     When I log in and give stuff
   }
   allow( Post ).to receive( :has_attached_file ).and_return false
-  VCR.use_cassette('aws_cucumber', :match_requests_on => [:method] ) do 
-      @post = build(:post, creator_id: @current_user.id, latitude: "47.6097", longitude: '-122.3331', description: "okkk" ) 
+  VCR.use_cassette('aws_cucumber', :match_requests_on => [:method] ) do
+      @post = build(:post, creator_id: @current_user.id, latitude: "47.6097", longitude: '-122.3331', description: "okkk" )
    end
    allow(Post).to receive( :new ).and_return( @post )
    allow(Post).to receive( :save ).and_call_original
 
-    within('#give-stuff-form') do 
+    within('#give-stuff-form') do
       expect(page).to have_field 'description'
       fill_in 'description', with: "okkk"
       click_button 'Give this stuff!'
     end 
-  sleep(2)
-  
+
   expect(page).to have_text('Please verify your email to give stuff')
   click_link('Sign Out')
   @shoes = Post.first
@@ -244,7 +243,7 @@ When(/^I sign in I should not be able to dib Jack's shoes or post an item\.$/) d
   }
   sleep(2)
   expect(page).to have_text('Please verify your email to dib')
-  
+
 end
 #"/user/signup"
 
@@ -261,4 +260,3 @@ Then(/^I should have an account created and my email should be marked verified\.
    expect(User.count).to eq 1
    expect(User.last.verified_email).to eq true
  end
-
