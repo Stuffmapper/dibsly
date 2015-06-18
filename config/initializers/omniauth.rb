897aa647ca196866a05a9115eae340c228ba8584
Ben Patterson <benthomaspatterson@gmail.com>
2015-06-18 16:42:31 -0700

    facebok

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Rails.application.secrets.facebok_app_id,
+  provider :facebook, Rails.application.secrets.facebook_app_id,
   Rails.application.secrets.facebook_secret
   provider :google_oauth2, Rails.application.secrets.google_client_id,
   Rails.application.secrets.google_client_secret
