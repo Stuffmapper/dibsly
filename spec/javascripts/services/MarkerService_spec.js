87acf2586a62e1e8e438f4d0a3f0ada361d2696a
Ben Patterson <benthomaspatterson@gmail.com>
2015-09-13 16:57:21 -0700

    some tests done

--- spec/javascripts/services/MarkerService_spec.js
+++ spec/javascripts/services/MarkerService_spec.js
@@ -2,14 +2,15 @@
 describe('MarkerService', function() {
 
 
-  var MarkerService, gmarker, markers, mockLocalService;
+  var mockMapsService,MarkerService, gmarker,gmap, markers, mockLocalService, $q, $rootScope;
   //Mocking google
   //TODO replace with a better Mocking strategy
   window.google = { maps: {
       LatLng: function(){ return {} },
       event: { addListener: function(){} },
       Marker: function(){ return {thing: 'marker'} }
-    }
+    },
+    load: function(){ }//Again todo ..beter mocking
   };
 
 
@@ -18,12 +19,16 @@ describe('MarkerService', function() {
 
   var setupController = function(){
 
-    inject(function(_MarkerService_, _LocalService_){
+    inject(function(_MapsService_, _MarkerService_, _LocalService_, _$q_, _$rootScope_ ){
       // The injector unwraps the underscores (_) from around the parameter names when matchin
       MarkerService = _MarkerService_;
       mockLocalService = _LocalService_;
+      mockMapsService = _MapsService_;
+      $rootScope = _$rootScope_;
+      $q = _$q_;
     });
-    gmarker = jasmine.createSpyObj('gmarker',['setIcon', ])
+    gmarker = jasmine.createSpyObj('gmarker',['setIcon' ]);
+    gmap = jasmine.createSpyObj('gmap',['panTo' ]);
     markers = {
      1:{id:1, dibber:'Jack', updated_at: new Date('2012-11-25'), marker: gmarker },
      2:{id: 2, dibber:'Jack', updated_at: new Date('2014-11-22'), marker: gmarker, category: 'books'},
@@ -32,6 +37,8 @@ describe('MarkerService', function() {
      5:{id:5, dibber:'Jack', updated_at: new Date('2014-11-23'), marker: gmarker, },
      6:{id:6, marker:gmarker}
     };
+    spyOn(mockMapsService,'loadMap').and.returnValue( $q( function(resolve, reject){ resolve(gmap); }) ) 
+    spyOn(mockMapsService,'newMapMarker').and.returnValue( $q( function(resolve, reject){ resolve(gmarker); }) ) 
   };
 
 
@@ -88,15 +95,21 @@ describe('MarkerService', function() {
       expect(MarkerService.markers[1].url_image).toEqual(newinfo.url_image);
     });
 
-    it('creates a marker ', function() {
+    it('creates a marker ', function(done) {
       setupController();
       var info = {id:1, url: 'this is a url'}
       var newinfo = {id:1, url_image: 'this is a url'}
       expect(MarkerService.markers[1]).toBeUndefined();
       MarkerService.setMarker(info);
       expect(MarkerService.markers[1].id).toEqual(info.id);
-      MarkerService.setMarker(newinfo);
-      expect(MarkerService.markers[1].marker).not.toBeUndefined();
+      MarkerService.setMarker(newinfo)
+      .then(
+        function(){
+          expect(MarkerService.markers[1].marker).not.toBeUndefined();
+          done();
+        }
+      )
+      $rootScope.$digest();
     });
 
     it('creates a marker with show dib and show edit functions', function() {
