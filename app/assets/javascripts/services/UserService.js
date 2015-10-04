d2d716553ca0d46a68fbb8719e3680039b118c10
Ben Patterson <benthomaspatterson@gmail.com>
2015-10-04 00:22:00 -0700

    added modified UserService.check

--- app/assets/javascripts/services/UserService.js
+++ app/assets/javascripts/services/UserService.js
@@ -5,60 +5,84 @@
 
   factories.factory('UserService', [
     '$http', '$q', 'LocalService', function($http, $q, LocalService) {
+
+      var self = this;
+      self.checking = false;
+      self.checkingQueue = [];
       return {
         login: function(username, password, callback) {
-          var self = this;
+          //TODO switch to promises for consistency
+          var that = this;
           loginData = {
             username: username,
             password: password
           };
           return $http.post('/api/sessions/create', loginData).success(function(data) {
             if (data && data.user) {
-              self.currentUser = data.user;
-              self.token = data.token;
+              that.currentUser = data.user;
+              that.token = data.token;
               LocalService.set('sMToken', JSON.stringify(data));
             } else {
               console.log(data);
               LocalService.unset('sMToken');
-              self.currentUser = false;
+              that.currentUser = false;
             }
             return callback(null, data);
           }).error(function(err) {
             console.log(err);
-            self.currentUser = false;
+            that.currentUser = false;
             return callback(err);
           });
         },
         logout: function(callback) {
-          var self = this;
+          //TODO switch to promises for consistency
+          var that = this;
           localStorage.clear();
+          //should this passed into LocalService?
           return $http.get('/api/log_out').success(function(data) {
-            self.currentUser = false;
+            that.currentUser = false;
             return callback(null, data);
           }).error(function(err) {
             return callback(err);
           });
         },
         check: function(){
-          var self = this;
-          return $q( function(resolve, reject){
-            var user;
-            user = LocalService.getJSON('sMToken');
+          var that = this;
+          user = LocalService.getJSON('sMToken');
+          var defer = $q.defer();
+          self.checkingQueue.push(defer)
+          if (!self.checking){
+            self.checking = true;
             if (user) {
-              return $http.get('/api/auth/check?token=' + user.token)
-              .success(
-                function(data) {
-                self.currentUser = user.user;
-                resolve(data)
-              }).error(function(err) {
-                self.currentUser = false;
-                reject(err);
+              $http.get('/api/auth/check?token=' + user.token)
+              .success(function(data) {
+                that.currentUser = user.user;
+                while(self.checkingQueue.length > 0){
+                  var prom = self.checkingQueue.pop();
+                  prom.resolve(that.currentUser);
+                };
+                that.checking = false;
+              })
+              .error(function(err) {
+                that.currentUser = false;
+                while(self.checkingQueue.length > 0){
+                  var prom = self.checkingQueue.pop();
+                  prom.reject(self.currentUser)
+                }
               });
             } else {
-              self.currentUser = false;
-              reject('error');
+              that.currentUser = false;
+              console.warn('user not valid')
+              reject(that.currentUser);
             }
-          });
+          } 
+          return defer.promise;
+
+        },
+        getCurrentUser: function(){
+          // var that = this;
+          // if( ) 
+
         }
       };
     }
