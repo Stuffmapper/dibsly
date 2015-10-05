c43256e83e45b28499ad108a9e2b2c475211722a
Ben Patterson <benthomaspatterson@gmail.com>
2015-10-04 18:53:27 -0700

    object to avoid duplicatione

--- app/assets/javascripts/directives/mywantsbtn.js
+++ app/assets/javascripts/directives/mywantsbtn.js
@@ -19,7 +19,7 @@ directives.directive('mywantsbtn', function() {
     controller: [
       '$scope', function($scope) {
         $scope.msg = function(post) {
-          console.log(post);
+          //console.log(post);
           if (post.noMessages()){
             $scope.color = 'green';
             return 'Send a message! (20min remaining)';
