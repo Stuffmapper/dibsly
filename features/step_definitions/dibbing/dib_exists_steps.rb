acf19a2b64558d1789d27b1254b46b61d17a701f
Ben Patterson <benthomaspatterson@gmail.com>
2015-10-06 16:53:19 -0700

    test hack 2

--- features/step_definitions/dibbing/dib_exists_steps.rb
+++ features/step_definitions/dibbing/dib_exists_steps.rb
@@ -11,6 +11,7 @@ end
 
 
 Then(/^I should not be able to dib the "(.*?)"$/) do |description|
+  page.execute_script "window.scrollBy(0,10000)"
   find(:button, 'I want this').click
   post = Post.find_by_description(description)
   expect(post.available_to_dib?).to eq true 
