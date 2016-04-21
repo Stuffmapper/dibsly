06fbd6962a4d168fc57dbf1dd073ddd2eeb4c85e
RyantheFarmer <ryan.the.farmer@gmail.com>
2016-04-21 11:47:07 -0700

    tabbing issue

--- features/step_definitions/user_accounts_steps.rb
+++ features/step_definitions/user_accounts_steps.rb
@@ -166,116 +166,114 @@ Then(/^I should receive an email with a link to reset my password$/) do
     @email = MandrillMailer::deliveries.detect{
         |mail| mail.template_name == 'password-reset' &&
         mail.message['to'].any? { |to| to[:email] == @user.email }}
-        puts @email
-        puts @user.email
-        sleep(2)
-        expect(@email).to_not be(nil)
-        sleep(2)
-        @link = @email.message["global_merge_vars"].select { |var| var['name'] == "CHANGEPASSWORD" }[0]["content"]
-        sleep(2)
-        expect(@link).to have_text(   'user/email/reset/' + User.find_by_email(@user.email).password_reset_token )
-        sleep(2)
+    sleep(2)
+    expect(@email).to_not be(nil)
+    sleep(2)
+    @link = @email.message["global_merge_vars"].select { |var| var['name'] == "CHANGEPASSWORD" }[0]["content"]
+    sleep(2)
+    expect(@link).to have_text(   'user/email/reset/' + User.find_by_email(@user.email).password_reset_token )
+    sleep(2)
 
+end
+
+When(/^I follow the reset password link and set my new password to "(.*?)"$/) do |arg1|
+    visit('#/menu/user/email/reset/' + User.find_by_email(@user.email).password_reset_token )
+
+    expect(page).to have_content("Change Your Password")
+    fill_in 'password', with: arg1
+    fill_in 'password_confirmation', with: arg1
+    click_button 'Change Password'
+end
+
+
+Then(/^I should be able to login with my username and "(.*?)"$/) do |arg1|
+    sleep(2)
+    if first(:link, 'Sign Out') != nil
+        first(:link, 'Sign Out').click
     end
 
-    When(/^I follow the reset password link and set my new password to "(.*?)"$/) do |arg1|
-        visit('#/menu/user/email/reset/' + User.find_by_email(@user.email).password_reset_token )
-
-        expect(page).to have_content("Change Your Password")
-        fill_in 'password', with: arg1
-        fill_in 'password_confirmation', with: arg1
-        click_button 'Change Password'
+    sleep(2)
+    first(:link, 'Sign In').click
+    fill_in 'username', with: @user.username
+    fill_in 'password', with: arg1
+    within('.signin') do
+        click_button 'Sign In'
     end
+    expect(page).to have_text("You have been signed in")
+end
+
+Then(/^I should be able to see "(.*?)"$/) do |arg1|
+    expect(page).to have_text(arg1)
+end
 
 
-    Then(/^I should be able to login with my username and "(.*?)"$/) do |arg1|
-        sleep(2)
-        if first(:link, 'Sign Out') != nil
-            first(:link, 'Sign Out').click
-        end
 
-        sleep(2)
-        first(:link, 'Sign In').click
-        fill_in 'username', with: @user.username
-        fill_in 'password', with: arg1
-        within('.signin') do
-            click_button 'Sign In'
-        end
-        expect(page).to have_text("You have been signed in")
+When(/^I try to login, I should be able to use my email in place of username$/) do
+    visit('/')
+    first(:link,'Sign In').click
+    fill_in 'username', with: @current_user.email
+    fill_in 'password', with: @current_user.password
+    within('.signin') do
+        click_button 'Sign In'
     end
+    sleep(1)
+    expect(page).to have_text("You have been signed in")
 
-    Then(/^I should be able to see "(.*?)"$/) do |arg1|
-        expect(page).to have_text(arg1)
+end
+
+##EMAIL VERIFICATION
+
+Then(/^I should receive a welcome email$/) do
+    sleep(1)
+    @current_user = User.last
+    @email = MandrillMailer::deliveries.detect{ |mail| mail.template_name == 'email-verification' && mail.message['to'].any? { |to| to[:email] == @current_user.email }}
+    expect(@email).to_not be_nil
+    # email.message["global_merge_vars"]
+
+    #expect(current_email.body).to have_text("Welcome to Stuffmapper!" )
+
+end
+
+When(/^I follow the link in the welcome email$/) do
+    expect(@current_user.verified_email).to eq false
+    link = @email.message["global_merge_vars"].select { |var| var['name'] == "CONFIRMEMAIL" }[0]["content"]
+    expect(link).to have_text( '#/users/email/' + User.find_by_email(@current_user.email).verify_email_token )
+    visit link
+    sleep(2)
+    @current_user.reload
+    expect(@current_user.verified_email).to eq true
+end
+
+Then(/^I should be able to post an item and dib Jacks shoes$/) do
+    visit '/'
+    if first(:link, 'Sign Out') != nil
+        first(:link, 'Sign Out').click
     end
+    sign_in @current_user
+    center_map_to_post Post.last
+    visit('#/menu/giveStuff')
+    sleep(2)
+    make_file_input_interactable
+    page.attach_file('give-stuff-file-1', Rails.root.join("spec/factories/shoes.png"), :visible=>false)
+    steps %{
+        Then I should be able to put  "These are some awesome kicks" in the description field
+
+    }
+    @shoes = Post.first
+    center_map_to_post @shoes
+
+    click_link 'Get Stuff'
+    page.all(".stuff-view")[1].click
+    page.execute_script "window.scrollBy(0,10000)"
+    page.first(:button, "Dib").click
+    sleep(2)
+    expect(page).to have_text('Dibbed')
+
+end
 
 
 
-    When(/^I try to login, I should be able to use my email in place of username$/) do
-        visit('/')
-        first(:link,'Sign In').click
-        fill_in 'username', with: @current_user.email
-        fill_in 'password', with: @current_user.password
-        within('.signin') do
-            click_button 'Sign In'
-        end
-        sleep(1)
-        expect(page).to have_text("You have been signed in")
-
-    end
-
-    ##EMAIL VERIFICATION
-
-    Then(/^I should receive a welcome email$/) do
-        sleep(1)
-        @current_user = User.last
-        @email = MandrillMailer::deliveries.detect{ |mail| mail.template_name == 'email-verification' && mail.message['to'].any? { |to| to[:email] == @current_user.email }}
-        expect(@email).to_not be_nil
-        # email.message["global_merge_vars"]
-
-        #expect(current_email.body).to have_text("Welcome to Stuffmapper!" )
-
-    end
-
-    When(/^I follow the link in the welcome email$/) do
-        expect(@current_user.verified_email).to eq false
-        link = @email.message["global_merge_vars"].select { |var| var['name'] == "CONFIRMEMAIL" }[0]["content"]
-        expect(link).to have_text( '#/users/email/' + User.find_by_email(@current_user.email).verify_email_token )
-        visit link
-        sleep(2)
-        @current_user.reload
-        expect(@current_user.verified_email).to eq true
-    end
-
-    Then(/^I should be able to post an item and dib Jacks shoes$/) do
-        visit '/'
-        if first(:link, 'Sign Out') != nil
-            first(:link, 'Sign Out').click
-        end
-        sign_in @current_user
-        center_map_to_post Post.last
-        visit('#/menu/giveStuff')
-        sleep(2)
-        make_file_input_interactable
-        page.attach_file('give-stuff-file-1', Rails.root.join("spec/factories/shoes.png"), :visible=>false)
-        steps %{
-            Then I should be able to put  "These are some awesome kicks" in the description field
-
-        }
-        @shoes = Post.first
-        center_map_to_post @shoes
-
-        click_link 'Get Stuff'
-        page.all(".stuff-view")[1].click
-        page.execute_script "window.scrollBy(0,10000)"
-        page.first(:button, "Dib").click
-        sleep(2)
-        expect(page).to have_text('Dibbed')
-
-    end
-
-
-
-    When(/^I sign in I should not be able to dib Jack's shoes or post an item\.$/) do
+When(/^I sign in I should not be able to dib Jack's shoes or post an item\.$/) do
     @current_user = User.last
     sign_in @current_user
     allow( Post ).to receive( :has_attached_file ).and_return false
